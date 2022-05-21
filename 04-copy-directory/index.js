4caf4e13edd76bedf22ececce77e5cc09a0fa716
OreskaG <Ctepanrfo@gmail.com>
2022-05-21 16:10:28 +0300

    init: start task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 
+const copiedFolderPath = path.join(__dirname, 'files-copy');
+const originFolderPath= path.join(__dirname,'files');
+fs.mkdir(copiedFolderPath,{recursive:true}, err => { if (err) throw err; });
+
+function copyDir(originFolderPath, copiedFolderPath) {
+  fs.readdir(originFolderPath, { withFileTypes: true }, (err, files)=> {
+    if (err) throw err;
+    files.forEach(file=> {
+      if (file.isFile()) {
+        fs.copyFile(path.join(originFolderPath, file.name), path.join(copiedFolderPath, file.name), err => {
+          if (err) throw err;
+        }); 
+      }
+    });
+  });
+}
+
+copyDir(originFolderPath, copiedFolderPath);

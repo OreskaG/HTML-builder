6f60bb3c59badae924ebbebac1501678f3d55c7d
OreskaG <Ctepanrfo@gmail.com>
2022-05-26 17:11:32 +0300

    fix: input '\n' bug

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,9 +7,11 @@ let writeFunction = fs.createWriteStream(fileDir, 'utf-8');
 
 stdout.write('Введите текст \n');
 stdin.on('data', data => {
-  let inputText = data.toString().slice(0, data.length-2);
+  // let inputText = data.toString().slice(0, data.length-2) + '\n';
+  let inputText = data.toString();
   if (inputText === 'exit') process.exit();
   writeFunction.write(inputText);
+  // fs.appendFile(fileDir, inputText, (err) => {if (err) throw err })
 });
 
 process.on('SIGINT', exit);
